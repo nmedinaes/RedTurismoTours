5f49885653ce13d97f340dc2d97a177e608e4e89
nmedinaes <nicolasmeddina789@gmail.com>
2022-08-02 23:59:45 -0500

    Añadir DbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,66 @@
+﻿using System.Data.Entity;
+
+namespace WpfEntityDb.Data
+{
+
+    public class DbContext : System.Data.Entity.DbContext
+    {
+        // El contexto se ha configurado para usar una cadena de conexión 'WpfEntityDbContext' del archivo 
+        // de configuración de la aplicación (App.config o Web.config). De forma predeterminada, 
+        // esta cadena de conexión tiene como destino la base de datos 'WpfEntityDb.Data.WpfEntityDbContext' de la instancia LocalDb. 
+        // 
+        // Si desea tener como destino una base de datos y/o un proveedor de base de datos diferente, 
+        // modifique la cadena de conexión 'DbContext'  en el archivo de configuración de la aplicación.
+        public DbContext()
+            : base("name=DbContext")
+        {
+        }
+
+        // Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
+        // sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        //public virtual DbSet<Raza> Razas { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Aquí haremos nuestras configuraciones con Fluent API.
+
+            // Especificar el nombre de una tabla.
+            //modelBuilder.Entity<Raza>().Map(m => m.ToTable("Raza"));   
+
+            /*
+            // establecer una primary key.
+            modelBuilder.Entity<Producto>().HasKey(c => c.Codigo);
+
+            // Definir un campo como requerida.
+            modelBuilder.Entity<Producto>().Property(c => c.Nombre).IsRequired();
+
+            // Definir el nombre de un campo.
+            modelBuilder.Entity<Producto>().Property(c => c.Nombre).HasColumnName("Nombre");
+
+            // Definir el tipo de un campo.
+            modelBuilder.Entity<Producto>().Property(c => c.Nombre).HasColumnType("varchar");
+
+            // Definir el orden de un campo.
+            modelBuilder.Entity<Producto>().Property(c => c.Nombre).HasColumnOrder(2);
+
+            // Definir el máximo de caracteres permitidos para un campo.
+            modelBuilder.Entity<Producto>().Property(c => c.Descripcion).HasMaxLength(100);
+
+            // indicar que no se debe mapear una pripiedad a la base de datos.
+            modelBuilder.Entity<Producto>().Ignore(c => c.CodigoIso);   */
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+}
